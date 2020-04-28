9b4ffb42d9fd4c95ac35c78068f199342e19c6b3
panlourenco <lrooudrreingcoo@gmail.com>
2020-04-28 20:33:22 +0200

    exercise 3.1 done

---
+++ chapter-3-3-1.py
@@ -0,0 +1,6 @@
+# 3-1. Names: Store the names of a few of your friends in a list called names. Print each person’s name by accessing each element in the list, one at a time.
+
+friends =['Andre', 'Rodrigo', 'Vinicius']
+print(friends[0])
+print(friends[1])
+print(friends[2])
