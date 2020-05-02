ac1172f33a2a855f56a2249c11a0044ad0c392f6
panlourenco <lrooudrreingcoo@gmail.com>
2020-05-02 21:55:53 +0200

    Chap3 3-2 done

---
+++ chapter-3-3-3.py
@@ -0,0 +1,5 @@
+carbrands = ["Toyota car", "Harley Davidson motorcycle", "Land-Rover jeep"]
+message = "I would like to own a "
+print(message + carbrands[0] + ".")
+print(message + carbrands[1] + ".")
+print(message + carbrands[2] + ".")
