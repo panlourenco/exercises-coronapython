d0b56c67bf5a5caaf2c19d439b1fa9c31456a235
panlourenco <lrooudrreingcoo@gmail.com>
2020-05-05 19:32:52 +0200

    Chap 3-3-4 done

--- chapter-3-3-3.py
+++ chapter-3-3-3.py
@@ -1,5 +1,10 @@
 carbrands = ["Toyota car", "Harley Davidson motorcycle", "Land-Rover jeep"]
+carbrands.insert(0,'suzuki')
 message = "I would like to own a "
 print(message + carbrands[0] + ".")
 print(message + carbrands[1] + ".")
 print(message + carbrands[2] + ".")
+carbrands.append('ducatti')
+popped_carbrands = carbrands.pop()
+print(carbrands)
+print(popped_carbrands)
---
+++ chapter-3-3-4.py
@@ -0,0 +1,6 @@
+# Guest list
+
+guests= ['Paulina', 'Jorge', 'Peter']
+print("Hi, " + guests[0] + "!" + " Would you like to get together for dinner?")
+print("Hi, " + guests[1] + "!" + " Would you like to get together for dinner?")
+print("Hi, " + guests[2] + "!" + " Would you like to get together for dinner?")
