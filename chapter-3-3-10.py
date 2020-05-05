dc473f99a5f891fc22b24e0d15b433f57830b7a6
panlourenco <lrooudrreingcoo@gmail.com>
2020-05-05 21:00:49 +0200

    chap -3-3-10 done

---
+++ chapter-3-3-10.py
@@ -0,0 +1,12 @@
+#Every function
+
+languages = ['French', 'Polish', 'English', 'Portuguese']
+print(languages)
+
+languages[1] = "Spanish"
+del languages[0]
+print(languages)
+languages.insert(1, "Latin")
+print(languages)
+languages.append("Italian"
+)
