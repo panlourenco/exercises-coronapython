3523eae22072d4ba8ec5f4e4daddcb4b2a4f1688
panlourenco <lrooudrreingcoo@gmail.com>
2020-05-06 22:14:01 +0200

    chapter 3-3-11 done

---
+++ chapter-3-3-11.py
@@ -0,0 +1,9 @@
+# 3-11. Intentional Error:
+
+polishcities= ['Wroclaw', 'Krakow', 'Warsaw']
+# if I try to run the following command, I get an index error
+# print polishcities[3]
+
+#If I wanted to print the third element, so I should've done:
+
+print(polishcities[2])
