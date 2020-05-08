20f847f21f9e19c81f788b0c2875235976613719
panlourenco <lrooudrreingcoo@gmail.com>
2020-05-08 15:53:33 +0200

    chapter-4-4-1 done

---
+++ chapter-4-4-1.py
@@ -0,0 +1,7 @@
+pizzas = ['margherita', 'pepperoni', 'mozzarella']
+for pizza in pizzas:
+    print("I like " + pizza + " pizza!") 
+for pizza in pizzas:
+    print (pizza.title() + " is best the pizza, hands down!")
+
+print("\nI really love pizza!")
