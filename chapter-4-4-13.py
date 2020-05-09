b1bd0bafa4af4162e305c7d643ffef19461fb5b0
panlourenco <lrooudrreingcoo@gmail.com>
2020-05-09 07:43:34 +0200

    chapter-4-4-13 done

---
+++ chapter-4-4-13.py
@@ -0,0 +1,12 @@
+# 4-13. Buffet:
+
+foods = ('ribs', 'spaghetti', 'strogonnoff', 'zurek', 'chicken')
+print(foods)
+
+# checking that I can't assign a new value to an item in a tuple: 
+# foods[0] = 'cannoli'
+
+# revising menu
+foods = ('ribs', 'cannoli', 'strogonnoff', 'zurek', 'pork')
+for food in foods:
+    print(food)
