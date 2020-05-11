2de2a9547b30a63eb5b7c4d823861ef54a3f0cf2
panlourenco <lrooudrreingcoo@gmail.com>
2020-05-11 21:58:30 +0200

    chapter-6-6-1--3 done

---
+++ chapter-6-6-1.py
@@ -0,0 +1,12 @@
+# 6-1. Person:
+person = {
+    'first_name': 'Rodrigo',
+    'last_name': 'Lourenco',
+    'age': 31,
+    'city': 'Krakow',
+    }
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
---
+++ chapter-6-6-2.py
@@ -0,0 +1,24 @@
+# 6-2. Favorite Numbers:
+
+favorite_numbers = {
+    'raul': 42,
+    'josh': 23,
+    'alice': 7,
+    'hank': 1000000,
+    'maggie': 0,
+    }
+
+num = favorite_numbers['raul']
+print("Raul's favorite number is " + str(num) + ".")
+
+num = favorite_numbers['josh']
+print("Josh's favorite number is " + str(num) + ".")
+
+num = favorite_numbers['alice']
+print("Alice's favorite number is " + str(num) + ".")
+
+num = favorite_numbers['hank']
+print("Hank's favorite number is " + str(num) + ".")
+
+num = favorite_numbers['maggie']
+print("Maggie's favorite number is " + str(num) + ".")
