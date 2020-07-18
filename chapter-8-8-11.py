739a900df351e12f710729f2083dceaa13318cac
panlourenco <lrooudrreingcoo@gmail.com>
2020-07-18 22:06:54 +0200

    chapter-8-8-9--11 done

---
+++ chapter-8-8-11.py
@@ -0,0 +1,25 @@
+# Unchanged Magicians
+
+def show_magicians(magicians):
+
+    for magician in magicians:
+        print(magician.title())
+
+def make_great(magicians):
+    great_magicians = []
+    
+    while magicians:
+        magician = magicians.pop()
+        great_magician = magician + ' the Great.'
+        great_magicians.append(great_magician)
+    
+    for great_magician in great_magicians:
+        magicians.append(great_magician)
+
+    return magicians
+
+magicians = ['david copperfield', 'teller', 'david blaine']
+
+great_magicians = make_great(magicians[:])
+show_magicians(great_magicians)
+show_magicians(magicians)
---
+++ chapter-8-8-9.py
@@ -0,0 +1,10 @@
+# Magicians
+
+def show_magicians(magicians):
+
+    for magician in magicians:
+        print(magician.title())
+
+
+magicians = ['david copperfield', 'teller', 'david blaine']
+show_magicians(magicians)
